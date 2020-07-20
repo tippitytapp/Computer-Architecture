4b0655d7eb3c11ed245a80eaaf5253a93fd865bc
Marc Tapp <marc.tapp@gmail.com>
2020-07-20 15:55:06 -0400

    initial commit

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -30,6 +32,8 @@ class CPU:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, pc):
+        pass
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
