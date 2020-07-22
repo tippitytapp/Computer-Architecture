caa3110b22073b3610ca2b82fe431226681f1162
Marc Tapp <marc.tapp@gmail.com>
2020-07-22 12:52:26 -0400

    working on the call function still

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,14 +2,19 @@
 
 import sys
 
-HLT = 0b00000001
-LDI = 0b10000010
-PRN = 0b01000111
-MUL = 0b10100010
+HLT  = 0b00000001
+LDI  = 0b10000010
+PRN  = 0b01000111
+MUL  = 0b10100010
 PUSH = 0b01000101
-POP = 0b01000110
-'''* R7 is reserved as the stack pointer (SP)'''
-SP = 7
+POP  = 0b01000110
+CALL = 0b01010000
+RET  = 0b00010001
+CMP  = 0b10100111
+JEQ  = 0b01010101
+JMP  = 0b01010100
+JNE  = 0b01010110
+
 
 
 class CPU:
@@ -22,6 +27,7 @@ class CPU:
         self.reg = [0] * 8
         '''Also add properties for any internal registers you need, e.g. `PC`.'''
         self.pc = 0
+        '''* R7 is reserved as the stack pointer (SP)'''
         self.sp = 7
         '''Set up the branch table'''
         self.branchtable = {}
@@ -31,6 +37,24 @@ class CPU:
         self.branchtable[MUL] = self.mul
         self.branchtable[PUSH] = self.push
         self.branchtable[POP] = self.pop
+        self.branchtable[CALL] = self.call
+        self.branchtable[RET] = self.ret
+        self.branchtable[CMP] = self.cmp
+        self.branchtable[JEQ] = self.jeq
+        self.branchtable[JMP] = self.jmp
+        self.branchtable[JNE] = self.jne
+
+        '''Set Flags'''
+        self.fl = 0
+        self.resetFlags()
+
+    def resetFlags(self):
+        '''set during CMP if regA and regB is equal'''
+        self.E = 0 # set to false
+        '''set during CMP if regA is less than regB'''
+        self.L = 0 # set to false
+        '''set during CMP if regA is greater than regB'''
+        self.G = 0 # set to false
 
     def load(self):
         """Load a program into memory."""
@@ -100,48 +124,104 @@ class CPU:
         print()
 
 
-
+    '''Halt the CPU (and exit the emulator).'''
     def hlt(self):
         self.running = False
 
+    '''Set the value of a register to an integer.'''
     def ldi(self):
         operand_a = self.ram[self.pc + 1]
         operand_b = self.ram[self.pc + 2]
         self.reg[operand_a] = operand_b
-        self.pc += 3
+        # self.pc += 3
 
+    '''Print to the console the decimal integer value that is stored in the given register.'''
     def prn(self):
         to_prn = self.ram[self.pc + 1]
         print(self.reg[to_prn])
-        self.pc += 2
-    
+        # self.pc += 2
+
+    '''Multiply the values in two registers together and store the result in registerA.'''
     def mul(self):
         operand_a = self.ram[self.pc + 1]
         operand_b = self.ram[self.pc + 2]
         self.alu("MUL", operand_a, operand_b)
-        self.pc += 3
+        # self.pc += 3
     
+    '''Push the value in the given register on the stack.'''
     def push(self):
         tar_reg = self.ram[self.pc + 1]
         '''Decrement the `SP`.'''
         self.reg[self.sp] -= 1
         self.ram[self.reg[self.sp]] = self.reg[tar_reg]
-        self.pc += 2
+        # self.pc += 2
 
+    '''Pop the value at the top of the stack into the given register.'''
     def pop(self):
         tar_reg = self.ram[self.pc + 1]
         self.reg[tar_reg] = self.ram[self.reg[self.sp]]
         self.reg[self.sp] += 1
-        self.pc += 2
-    
+        # self.pc += 2
+
+    '''Calls a subroutine (function) at the address stored in the register.'''
+    def call(self):
+        nextpc = self.pc + 2
+        self.reg[self.sp] -= 1
+        self.ram[self.reg[self.sp]] = nextpc
+        pc = self.reg[self.ram[self.pc + 1]]
+        self.pc = pc
+
+    '''Return from subroutine.'''
+    def ret(self):
+        retpc = self.ram[self.pc]
+        self.reg[self.sp] += 1
+        self.pc = retpc
+
+    def cmp(self):
+        '''Compare the values in two registers.'''
+        self.resetFlags()
+        regA = self.reg[self.ram[self.pc + 1]]
+        regB = self.reg[self.ram[self.pc + 2]]
+
+        if regA == regB:
+            self.E = 1
+        elif regA < regB:
+            self.L = 1
+        else:
+            self.G = 1
+
+    def jeq(self):
+        '''If `equal` flag is set (true), jump to the address stored in the given register.'''
+        if self.E == 1:
+            self.pc = self.reg[self.ram[self.pc + 1]]
+        else:
+            self.pc += 2
+
+    def jmp(self):
+        '''Jump to the address stored in the given register.'''
+        jmp = self.reg[self.ram[self.pc + 1]]
+        self.pc = jmp
+
+    def jne(self):
+        '''If `E` flag is clear (false, 0), jump to the address stored in the given register.'''
+        if self.E == 0:
+            self.pc = self.reg[self.ram[self.pc + 1]]
+        else:
+            self.pc += 2
+
     def run(self):
         """Run the CPU."""
         self.running = True
+        self.reg[self.sp] = 244 # R7 in dec is 244
 
         while self.running:
             ir = self.pc
             inst = self.ram[ir]
+            inst_len = ((inst & 11000000) >> 6) + 1
+            flag_mask = (inst & 0b00010000)
             self.branchtable[inst]()
+            if flag_mask != 0b00010000:
+                self.pc += inst_len
             # if inst == HLT:
             #     self.hlt()
             # elif inst == PRN:
