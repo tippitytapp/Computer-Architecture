8c3f9e4c14a435c01c48601e52259980b521af14
Marc Tapp <marc.tapp@gmail.com>
2020-07-24 10:58:50 -0400

    day4 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,12 +10,11 @@ PUSH = 0b01000101
 POP  = 0b01000110
 CALL = 0b01010000
 RET  = 0b00010001
-CMP  = 0b10100111
-JEQ  = 0b01010101
-JMP  = 0b01010100
-JNE  = 0b01010110
+ADD  = 0b10100000
 
-
+'''
+Passing Examples -- Mult, Print8, sctest, stack
+'''
 
 class CPU:
     """Main CPU class."""
@@ -39,22 +38,7 @@ class CPU:
         self.branchtable[POP] = self.pop
         self.branchtable[CALL] = self.call
         self.branchtable[RET] = self.ret
-        self.branchtable[CMP] = self.cmp
-        self.branchtable[JEQ] = self.jeq
-        self.branchtable[JMP] = self.jmp
-        self.branchtable[JNE] = self.jne
-
-        '''Set Flags'''
-        self.fl = 0
-        self.resetFlags()
-
-    def resetFlags(self):
-        '''set during CMP if regA and regB is equal'''
-        self.E = 0 # set to false
-        '''set during CMP if regA is less than regB'''
-        self.L = 0 # set to false
-        '''set during CMP if regA is greater than regB'''
-        self.G = 0 # set to false
+        self.branchtable[ADD] = self.add
 
     def load(self):
         """Load a program into memory."""
@@ -87,10 +71,17 @@ class CPU:
 
     '''`ram_read()` should accept the address to read and return the value stored there.'''
     def ram_read(self, MAR):
-        return (self.ram[MAR])
+        try:
+            return (self.ram[MAR])
+        except IndexError:
+            print("index out of range for ram_read") 
+
     '''`ram_write()` should accept a value to write, and the address to write it to.'''
     def ram_write(self, MDR, MAR):
-        self.ram[MAR] = MDR
+        try:
+            self.ram[MAR] = MDR
+        except IndexError:
+            print("index out of range for ram_write")
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -123,30 +114,40 @@ class CPU:
 
         print()
 
+    def increment_pc(self, op_code):
+        add_to_pc = (op_code >> 6) + 1
+        self.pc += add_to_pc
 
     '''Halt the CPU (and exit the emulator).'''
     def hlt(self):
         self.running = False
+        sys.exit(1)
 
     '''Set the value of a register to an integer.'''
     def ldi(self):
         operand_a = self.ram[self.pc + 1]
         operand_b = self.ram[self.pc + 2]
         self.reg[operand_a] = operand_b
-        # self.pc += 3
+        self.increment_pc(LDI)
 
     '''Print to the console the decimal integer value that is stored in the given register.'''
     def prn(self):
         to_prn = self.ram[self.pc + 1]
         print(self.reg[to_prn])
-        # self.pc += 2
+        self.increment_pc(PRN)
 
     '''Multiply the values in two registers together and store the result in registerA.'''
     def mul(self):
         operand_a = self.ram[self.pc + 1]
         operand_b = self.ram[self.pc + 2]
         self.alu("MUL", operand_a, operand_b)
-        # self.pc += 3
+        self.increment_pc(MUL)
+
+    def add(self):
+        operand_a = self.ram[self.pc + 1]
+        operand_b = self.ram[self.pc + 2]
+        self.alu("ADD", operand_a, operand_b)
+        self.increment_pc(ADD)
     
     '''Push the value in the given register on the stack.'''
     def push(self):
@@ -154,60 +155,45 @@ class CPU:
         '''Decrement the `SP`.'''
         self.reg[self.sp] -= 1
         self.ram[self.reg[self.sp]] = self.reg[tar_reg]
-        # self.pc += 2
+        self.increment_pc(PUSH)
 
     '''Pop the value at the top of the stack into the given register.'''
     def pop(self):
         tar_reg = self.ram[self.pc + 1]
         self.reg[tar_reg] = self.ram[self.reg[self.sp]]
         self.reg[self.sp] += 1
-        # self.pc += 2
+        self.increment_pc(POP)
 
     '''Calls a subroutine (function) at the address stored in the register.'''
     def call(self):
-        nextpc = self.pc + 2
+        # try:
+            # ret_add = self.pc + 2
+            # # print(ret_add)
+            # self.reg[self.pc] -= 1
+            # addy = self.reg[self.pc]
+            # # print(addy)
+            # self.ram[addy] = ret_add
+            # reg_num = self.ram[self.pc + 1]
+            # # print(reg_num)
+            # subadd = self.reg[reg_num]
+            # # print(subadd)
+            # self.pc = subadd
         self.reg[self.sp] -= 1
-        self.ram[self.reg[self.sp]] = nextpc
-        pc = self.reg[self.ram[self.pc + 1]]
-        self.pc = pc
+        self.ram[self.reg[self.sp]] = self.pc + 2
+        addofsub = self.ram[self.pc + 1]
+        self.pc = self.reg[addofsub]
+        # except KeyError:
+        #     print(f'KeyError at {self.pc}')
+        #     sys.exit(1)
+        # except IndexError:
+        #     print(f'IndexError at {bin(self.sp)}')
+        #     sys.exit(1)
 
     '''Return from subroutine.'''
     def ret(self):
-        retpc = self.ram[self.pc]
+        self.pc = self.ram[self.reg[self.sp]]
         self.reg[self.sp] += 1
-        self.pc = retpc
 
-    def cmp(self):
-        '''Compare the values in two registers.'''
-        self.resetFlags()
-        regA = self.reg[self.ram[self.pc + 1]]
-        regB = self.reg[self.ram[self.pc + 2]]
-
-        if regA == regB:
-            self.E = 1
-        elif regA < regB:
-            self.L = 1
-        else:
-            self.G = 1
-
-    def jeq(self):
-        '''If `equal` flag is set (true), jump to the address stored in the given register.'''
-        if self.E == 1:
-            self.pc = self.reg[self.ram[self.pc + 1]]
-        else:
-            self.pc += 2
-
-    def jmp(self):
-        '''Jump to the address stored in the given register.'''
-        jmp = self.reg[self.ram[self.pc + 1]]
-        self.pc = jmp
-
-    def jne(self):
-        '''If `E` flag is clear (false, 0), jump to the address stored in the given register.'''
-        if self.E == 0:
-            self.pc = self.reg[self.ram[self.pc + 1]]
-        else:
-            self.pc += 2
 
     def run(self):
         """Run the CPU."""
@@ -217,11 +203,18 @@ class CPU:
         while self.running:
             ir = self.pc
             inst = self.ram[ir]
-            inst_len = ((inst & 11000000) >> 6) + 1
-            flag_mask = (inst & 0b00010000)
-            self.branchtable[inst]()
-            if flag_mask != 0b00010000:
-                self.pc += inst_len
+            # inst_len = ((inst & 11000000) >> 6) + 1
+            # pc_mask = (inst & 0b00010000)
+            # operA = self.reg[self.ram[self.pc + 1]]
+            # operB = self.reg[self.ram[self.pc + 2]]
+            # if pc_mask != 0b00010000:
+            #     self.pc += inst_len
+            try:
+                self.branchtable[inst]()
+            except KeyError:
+                print(f'KeyError at {self.reg[self.ram[inst]]}')
+                sys.exit(1)
+
             # if inst == HLT:
             #     self.hlt()
             # elif inst == PRN:
@@ -230,3 +223,4 @@ class CPU:
             #     self.ldi()
             # elif inst == MUL:
             #     self.mul()
+
